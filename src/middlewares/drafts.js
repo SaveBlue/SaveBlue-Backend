5e0386067dfa79e8022ecef1438bfd0023069b5d
Lan Sovinc <lan.sovinc@gmail.com>
2024-07-25 21:50:53 +0200

    drafts parsing from notification

--- src/middlewares/drafts.js
+++ src/middlewares/drafts.js
@@ -2,79 +2,84 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const openai = require("./openai");
 
+const parseSmsManually = (sms, isNlb) => {
+    let smsData = {}
+    if (isNlb) {
+        smsData.description = sms.split("EUR, ")[1].split(". Info")[0].substring(0, 32)
+        let date = sms.split("POS NAKUP ")[1].split(" ")[0].split(".")
+        smsData.date = new Date(date[2], date[1] - 1, date[0])
+        smsData.amount = parseInt(sms.split("znesek ")[1].split(" EUR")[0].replaceAll(",", "").replaceAll(".", ""))
+    } else {
+        smsData.description = sms.split(" €")[0].substring(0, 32)
+        smsData.date = Date.now();
+        smsData.amount = parseInt(sms.split(" €")[1].split(" ")[0].replaceAll(",", "").replaceAll(".", ""))
+    }
+    smsData.category1 = "Draft"
+    smsData.category2 = "Draft"
+    return smsData
+};
+
+const parseSmsWithAI = async (sms) => {
+    try {
+        const smsAI = await openai.createExpense(sms)
+        return {
+            description: smsAI.description,
+            date: new Date(smsAI.date),
+            amount: parseInt(smsAI.amount),
+            category1: smsAI.category1,
+            category2: smsAI.category2
+        };
+    } catch (e) {
+        throw new Error("AI SMS parsing failed.")
+    }
+};
 
 exports.createExpenseSMS = async (req, res, next) => {
 
-    let user = {}
-
     // Fetch user id and draftsAccount id
     try {
-        user = await User.findOne({'_id': req.params.tokenId}, 'draftsAccount')
+        const user = await User.findOne({'_id': req.params.tokenId}, 'draftsAccount')
         if (!user) {
             return res.status(404).json({
                 message: "No user with selected ID!"
             });
         }
-    }
-    catch (error){
+    } catch (error) {
         res.status(500).send({
             message: error.message || "An error occurred while fetching user!"
         });
     }
 
     // Extract data from sms
-    let sms = req.body.sms
-    let smsData = {}
-
     try {
-        if(sms.startsWith("POS NAKUP")){
-            if (req.body.ai){
-                try {
-                    const smsAI = await openai.createExpense(sms)
-                    smsData.description = smsAI.description
-                    smsData.date = new Date(smsAI.date)
-                    smsData.amount = parseInt(smsAI.amount)
-                    smsData.category1 = smsAI.category1
-                    smsData.category2 = smsAI.category2
-                }
-                catch (e){
-                    return res.status(400).json({
-                        message: "AI SMS parsing failed."
-                    });
-                }
-            }
-            else {
-                smsData.description = sms.split("EUR, ")[1].split(". Info")[0].substring(0, 32)
-                let date = sms.split("POS NAKUP ")[1].split(" ")[0].split(".")
-                smsData.date = new Date(date[2],date[1]-1, date[0])
-                smsData.amount = parseInt(sms.split("znesek ")[1].split(" EUR")[0].replaceAll(",", "").replaceAll(".", ""))
-                smsData.category1 = "Draft"
-                smsData.category2 = "Draft"
-            }
+        const sms = req.body.sms
+        const isNlb = sms.startsWith("POS NAKUP");
+        let smsData;
+
+        if (req.body.ai) {
+            smsData = await parseSmsWithAI(sms);
+        } else {
+            smsData = parseSmsManually(sms, isNlb);
         }
+
+        req.body.userID = user._id
+        req.body.accountID = user.draftsAccount._id
+        req.body.description = smsData.description || ""
+        req.body.date = smsData.date
+        req.body.amount = smsData.amount || 0
+        req.body.category1 = smsData.category1 || "Draft"
+        req.body.category2 = smsData.category2 || "Draft"
+
+        next();
+    } catch (error) {
+        res.status(500).send({message: error.message || "An error occurred while processing the request!"});
     }
-    catch (e){
-        return res.status(400).json({
-            message: "SMS not valid."
-        });
-    }
-
-    req.body.userID = user._id
-    req.body.accountID = user.draftsAccount._id
-    req.body.description = smsData.description || ""
-    req.body.date = smsData.date
-    req.body.amount = smsData.amount || 0
-    req.body.category1 = smsData.category1 || "Draft"
-    req.body.category2 = smsData.category2 || "Draft"
-
-    next();
-
 };
 
 
 // Do not allow users to create drafts in regular accounts
 exports.block = (req, res, next) => {
-    if(req.body.category1 === "Draft" || req.body.category2 && req.body.category2 === "Draft"){
+    if (req.body.category1 === "Draft" || (req.body.category2 && req.body.category2 === "Draft")) {
         return res.status(400).json({
             message: "Cannot create draft in regular account."
         });
