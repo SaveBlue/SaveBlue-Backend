6e797410066ea45b66d6d9149c510cd5c1e09500
Lan Sovinc <lan.sovinc@gmail.com>
2024-09-08 19:09:53 +0200

    fix imports

--- src/middlewares/drafts.js
+++ src/middlewares/drafts.js
@@ -1,6 +1,7 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import openai from './openai'
+
 const User = mongoose.model('User');
-const openai = require("./openai");
 
 const parseSmsManually = (sms, isNlb) => {
     let smsData = {}
